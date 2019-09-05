4eb7aad05c83afcb91da3c20d1425249c52a0693
Anwesh43 <anweshthecool0@gmail.com>
2019-09-05 12:57:43 +0530

    importing libraries and constants

--- circlerectscreenview/src/main/java/com/anwesh/uiprojects/circlerectscreenview/CircleRectScreenView.kt
+++ circlerectscreenview/src/main/java/com/anwesh/uiprojects/circlerectscreenview/CircleRectScreenView.kt
@@ -4,3 +4,17 @@ package com.anwesh.uiprojects.circlerectscreenview
  * Created by anweshmishra on 05/09/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<String> = arrayOf("#e67e22", "#2980b9", "#2ecc71", "#e74c3c", "#1abc9c")
+val scGap : Float = 0.01f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+
