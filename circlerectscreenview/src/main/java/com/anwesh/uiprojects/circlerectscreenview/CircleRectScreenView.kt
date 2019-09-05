63e5f8a4c39ed1b1cf7b6fe4a80afb0cc8f0e116
Anwesh43 <anweshthecool0@gmail.com>
2019-09-05 13:16:47 +0530

    adding CircleRectScreenView class

--- circlerectscreenview/src/main/java/com/anwesh/uiprojects/circlerectscreenview/CircleRectScreenView.kt
+++ circlerectscreenview/src/main/java/com/anwesh/uiprojects/circlerectscreenview/CircleRectScreenView.kt
@@ -53,3 +53,21 @@ fun Canvas.drawCRSNode(i : Int, scale : Float, sc : Float, paint : Paint) {
     drawCircleRectScreen(size, scale, sc, paint)
     restore()
 }
+
+class CircleRectScreenView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
