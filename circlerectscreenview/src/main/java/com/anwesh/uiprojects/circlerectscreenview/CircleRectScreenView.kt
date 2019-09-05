10caa844a86c9f9eda8c1d756966d565c237ca0b
Anwesh43 <anweshthecool0@gmail.com>
2019-09-05 13:15:08 +0530

    adding Canvas extension method

--- circlerectscreenview/src/main/java/com/anwesh/uiprojects/circlerectscreenview/CircleRectScreenView.kt
+++ circlerectscreenview/src/main/java/com/anwesh/uiprojects/circlerectscreenview/CircleRectScreenView.kt
@@ -10,6 +10,7 @@ import android.graphics.Paint
 import android.graphics.RectF
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Path
 import android.app.Activity
 import android.content.Context
 
@@ -17,7 +18,38 @@ val colors : Array<String> = arrayOf("#e67e22", "#2980b9", "#2ecc71", "#e74c3c",
 val scGap : Float = 0.01f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
+val sizeFactor : Float = 2.5f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawRectScreen(x : Float, y : Float, size : Float, sc1 : Float, sc2 : Float, paint : Paint) {
+    var w : Float = size
+    if (sc2 > 0f) {
+        w = size * sc2
+    }
+    drawRect(RectF(x + size * sc1, y - size / 2, x + size, y + size / 2), paint)
+}
+fun Canvas.drawCircleRect(size : Float, sc1 : Float, sc2 : Float, paint : Paint) {
+    val path : Path = Path()
+    path.addCircle(size / 2, 0f, size / 2, Path.Direction.CW);
+    clipPath(path)
+    drawRectScreen(0f, 0f, size, sc1, sc2, paint)
+}
+
+fun Canvas.drawCircleRectScreen(size : Float, sc1 : Float, sc2 : Float, paint : Paint) {
+    drawRectScreen(-size, 0f, size, sc1.divideScale(0, 2), sc2.divideScale(0, 2), paint)
+    drawCircleRect(size, sc1, sc2, paint)
+}
+
+fun Canvas.drawCRSNode(i : Int, scale : Float, sc : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val size : Float = Math.min(w, h) / sizeFactor
+    paint.color = Color.parseColor(colors[i])
+    save()
+    translate(w / 2, h / 2)
+    drawCircleRectScreen(size, scale, sc, paint)
+    restore()
+}
